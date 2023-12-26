54e47208408daf708dbb834c90f4dfa36bfb0512
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2023-12-26 09:33:55 -0300

    att

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+
+  //1 - Variaveis 
+  const name: string = "Jonatas";
+  const age: number = 26;
+  const isWorking: boolean = true;
+
+  //2 - funções
+  const userGreeting = (name: string): string => {
+    return `Olá, ${name}!`;
+  };
+
+
   return (
     <div className="App">
       <h1>Typescript com React</h1>
+      <h2>Nome: {name}</h2>
+      <p>Idade: {age}</p>
+      {isWorking && (
+        <div>
+          <p>Está trabalhando!</p>
+        </div>
+      )}
+      <h3>{userGreeting(name)}</h3>
+
     </div>
   );
 }

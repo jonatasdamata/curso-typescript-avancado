62a0053c1548f1891c957a7ffff9f53a29a8a219
Jonatas da  Mata <jonatasdamata1@hotmail.com>
2024-01-12 09:40:17 -0300

    work in progess: finalizando o código js.  da seção15

--- 15_ES6_Moderno/sripts.js
+++ 15_ES6_Moderno/sripts.js
@@ -44,3 +44,63 @@ const arrownSum = (a, b) => {
 }
 
 console.log(sum(5,5))
+
+
+//3 - filter
+const arr = [ 1, 2, 3 , 4, 5 ]
+
+const highNumers = arr.filter((n) => {
+    if(n >= 3) {
+        return n
+    }
+})
+
+console.log(highNumers)
+
+
+//4 - map
+const products = [
+    { name: 'Camisa', price: 10.99, category: 'Roupas' },
+    { name: 'Chaleira Elétrica', price: 89.99, ctaegory: 'Eletro' },
+    { name: 'Fogão', price: 399.99, category: 'Eletro' },
+    { name: 'Calça Jenas', price: 110.99, category: 'Roupas' },
+]
+
+products.map((products) => {
+    if (products.category === 'Roupas') {
+        products.onSale = true
+    }
+})
+
+console.log(products)
+
+
+//5 - template literals
+const userName = "Jonatas"
+const age = 26
+
+console.log(`O nome do usuário é: ${userName}, e a sua idade é: ${age}`)
+
+// 6 - destructing 
+
+// 7 - spread operator
+
+
+// 8 - classes
+class Product {
+    constructor(name, price) {
+        this.name = name
+        this.price = price
+    }
+
+productEithDescount(discount) {
+    return this.price * ((100 - discount) / 100)
+}
+}
+
+const shirt = new Product('Camisa gola V', 20)
+
+console.log(shirt.name)
+console.log(shirt.productEithDescount(10))
+
+// 9 - herança
